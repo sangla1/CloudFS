98ee0dfe7a8d2b3a485bfe9dc831a33ab52202f0
Younghoon Jeon <yj2231@columbia.edu>
2012-05-12 18:02:27 -0400

    update

--- cloudfs.py
+++ cloudfs.py
@@ -1,123 +1,90 @@
 #!/usr/bin/env python
 
 from collections import defaultdict
-from errno import ENOENT
+from errno import *
 from stat import S_IFDIR, S_IFLNK, S_IFREG
 from sys import argv, exit
 from time import time
 
 from fusepy.fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 
-
-class Memory(LoggingMixIn, Operations):
-    """Example memory filesystem. Supports only one level of files."""
+class CloudFS(LoggingMixIn, Operations):
     
     def __init__(self):
-        self.files = {}
-        self.data = defaultdict(str)
-        self.fd = 0
-        now = time()
-        self.files['/'] = dict(st_mode=(S_IFDIR | 0755), st_ctime=now,
-            st_mtime=now, st_atime=now, st_nlink=2)
+        self.maxFiles = 1024
+        self.files = [None] * self.maxFiles
         
     def chmod(self, path, mode):
-        self.files[path]['st_mode'] &= 0770000
-        self.files[path]['st_mode'] |= mode
-        return 0
+        raise FuseOSError(EPERM)
 
     def chown(self, path, uid, gid):
-        self.files[path]['st_uid'] = uid
-        self.files[path]['st_gid'] = gid
+        raise FuseOSError(EPERM)
     
     def create(self, path, mode):
-        self.files[path] = dict(st_mode=(S_IFREG | mode), st_nlink=1,
-            st_size=0, st_ctime=time(), st_mtime=time(), st_atime=time())
-        self.fd += 1
-        return self.fd
+        raise FuseOSError(EPERM)
     
     def getattr(self, path, fh=None):
-        if path not in self.files:
+        now = time()
+        if path == '/':            
+            return dict(st_mode=(S_IFDIR | 0755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=2)
+        else:
             raise FuseOSError(ENOENT)
-        st = self.files[path]
-        return st
     
     def getxattr(self, path, name, position=0):
-        attrs = self.files[path].get('attrs', {})
-        try:
-            return attrs[name]
-        except KeyError:
-            return ''       # Should return ENOATTR
-    
+        raise FuseOSError(EPERM)
+
     def listxattr(self, path):
-        attrs = self.files[path].get('attrs', {})
-        return attrs.keys()
+        raise FuseOSError(EPERM)
     
     def mkdir(self, path, mode):
-        self.files[path] = dict(st_mode=(S_IFDIR | mode), st_nlink=2,
-                st_size=0, st_ctime=time(), st_mtime=time(), st_atime=time())
-        self.files['/']['st_nlink'] += 1
+        raise FuseOSError(EPERM)
     
     def open(self, path, flags):
-        self.fd += 1
-        return self.fd
+        raise FuseOSError(EPERM)
     
     def read(self, path, size, offset, fh):
-        return self.data[path][offset:offset + size]
+        raise FuseOSError(EPERM)
     
     def readdir(self, path, fh):
-        return ['.', '..'] + [x[1:] for x in self.files if x != '/']
+        print 'readdir : ', path, fh
+        dents = []
+        return ['.', '..'] + dents
     
     def readlink(self, path):
-        return self.data[path]
+        raise FuseOSError(EPERM)
     
     def removexattr(self, path, name):
-        attrs = self.files[path].get('attrs', {})
-        try:
-            del attrs[name]
-        except KeyError:
-            pass        # Should return ENOATTR
+        raise FuseOSError(EPERM)
     
     def rename(self, old, new):
-        self.files[new] = self.files.pop(old)
+        raise FuseOSError(EPERM)
     
     def rmdir(self, path):
-        self.files.pop(path)
-        self.files['/']['st_nlink'] -= 1
+        raise FuseOSError(EPERM)
     
     def setxattr(self, path, name, value, options, position=0):
-        # Ignore options
-        attrs = self.files[path].setdefault('attrs', {})
-        attrs[name] = value
-    
+        raise FuseOSError(EPERM)
+
     def statfs(self, path):
-        return dict(f_bsize=512, f_blocks=4096, f_bavail=2048)
+        raise FuseOSError(EPERM)
     
     def symlink(self, target, source):
-        self.files[target] = dict(st_mode=(S_IFLNK | 0777), st_nlink=1,
-            st_size=len(source))
-        self.data[target] = source
+        raise FuseOSError(EPERM)
     
     def truncate(self, path, length, fh=None):
-        self.data[path] = self.data[path][:length]
-        self.files[path]['st_size'] = length
+        raise FuseOSError(EPERM)
     
     def unlink(self, path):
-        self.files.pop(path)
+        raise FuseOSError(EPERM)
     
     def utimens(self, path, times=None):
-        now = time()
-        atime, mtime = times if times else (now, now)
-        self.files[path]['st_atime'] = atime
-        self.files[path]['st_mtime'] = mtime
-    
-    def write(self, path, data, offset, fh):
-        self.data[path] = self.data[path][:offset] + data
-        self.files[path]['st_size'] = len(self.data[path])
-        return len(data)
+        raise FuseOSError(EPERM)
 
+    def write(self, path, data, offset, fh):
+        raise FuseOSError(EPERM)
 
 if __name__ == "__main__":
     if len(argv) != 2:
         print 'usage: %s <mountpoint>' % argv[0]
         exit(1)
-    fuse = FUSE(Memory(), argv[1], foreground=True)
+    fuse = FUSE(CloudFS(), argv[1], foreground=True)
